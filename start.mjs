4fb2b80729e358495f464049b093cd523ce68274
Mark Lane <mark.lane@students.powerscatholic.org>
2026-04-28 20:03:28 +0000

    update

--- start.mjs
+++ start.mjs
@@ -6,4 +6,9 @@ if (!process.env.HOST) {
   process.env.HOST = process.env.NITRO_HOST;
 }
 
+// Ensure Nitro uses Render's provided PORT if present
+if (process.env.PORT && !process.env.NITRO_PORT) {
+  process.env.NITRO_PORT = process.env.PORT;
+}
+
 await import('./.output/server/index.mjs');
